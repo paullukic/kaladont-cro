a19c73e8d6d8e6c0e26de3589ba3206ab88eced5
Paul <paul.lukic94@gmail.com>
2021-12-11 12:15:22 +0100

    added threading

--- niz.py
+++ niz.py
@@ -1,18 +1,24 @@
 # Zanima nas koji je najduži niz riječi koji se može odigrati u "Kalodontu". 
 # Niz može početi s bilo kojom riječi, a prva dva slova svake iduće riječi moraju biti jednaka završetku prethodne riječi. 
 # Dozvoljene su sve riječi iz liste od oko 160 tisuća riječi pisanih malim slovima (a-z, znak -). 
-import random
 import sys
+import os
+import threading
+import time
+
 sys.setrecursionlimit(10**6)
 
 file1 = open('words.txt', 'r')
 Lines = file1.readlines()
-
-kaladont = []
-kaladonts = []
-
 for i in range(len(Lines)):
     Lines[i] = Lines[i].strip()
+
+kaladonts = {}
+
+class allKaladonts:
+    def __init__(self, firstWord, kaladont=None):
+        self.firstWord = firstWord
+        self.kaladont = kaladont or []
 
 def lastTwo(word):
     return word[-2:]
@@ -25,48 +31,57 @@ def find_max_list(lista):
     return max(list_len)
 
 # funckija za pronalazenje kaladonta
-def findWord(word, array, counter):
-    for i in range(len(array)):
-        if firstTwo(array[i]) == lastTwo(word) and counter < 1:
-            kaladont.append(array[i])
-            starterWord = array[i];
+def findWord(word, array, counter, firstWord):
+    words = array.copy()
+    for i in range(len(words)):
+        if firstTwo(words[i]) == lastTwo(word) and counter < 1:
             counter += 1
-
-            newArray = [x for x in array if x != array[i]]
-            findWord(kaladont[-1], newArray, 0)
-        
+            kaladonts[firstWord].kaladont.append(words[i])
+            filteredWords = [x for x in words if x != words[i]]
+            findWord(words[i], filteredWords, 0, firstWord)
         else:
             continue
-count = 0
+
+def getLongestList(kaladonts):
+    # pronadji index najduzeg niza
+    listKal = [i.kaladont for i in kaladonts]
+
+    longest = find_max_list(listKal)
+
+    # postavi najduzi niz u varijablu
+    longestKaladont = listKal[longest]
+
+    # ispisi najduzi niz u kaladont.txt
+    for i in range(len(longestKaladont)):
+        with open('kaladont.txt', 'a') as f:
+            f.write(longestKaladont[i] + ' ')
+copyLines = Lines.copy()
 # za svaku rec u tekstu, proveravamo da li su prva dva slova jednaka zavrsetku prethodne reci.
-for i in range(len(Lines)):
-    # izvuci rec 
-    newWord = Lines[i]
+def main(Lines, threadCounter, kaladonts):
 
-    # dodaj rec u listu kaladont
-    kaladont.append(newWord)
+    for i in range(len(Lines)):
+        if(len(copyLines) < 1):
+            getLongestList(kaladonts)
+            break
 
-    # izbaci rec iz liste
-    Lines.remove(newWord)
+        else:
+            # izvuci rec 
+            word = copyLines[0]
+            copyLines.remove(word)
+            # pokreni funkciju za pronalazenje kaladonta
+            print('Thread: ' + str(threadCounter) + ' - ' + word)
 
-    # pokreni funkciju za pronalazenje kaladonta
-    findWord(newWord, Lines, 0)
+            kaladonts[word] = allKaladonts(word, [])
+            findWord(word, Lines, 0, word)
 
-    # dodaj kaladont u listu kaladonts
-    kaladonts.append(kaladont)
+            print('Thread: ' + str(threadCounter) + ' - ' + word + ' - done')
+            print('Kaladont: ', kaladonts[word].kaladont)
 
-    # ocisti kaladont listu
-    kaladont = []
-    count += 1
-    print('Zavrsenih reci: ',count)
 
-# pronadji index najduzeg niza
-longest = find_max_list(kaladonts)
+    time.sleep(1)
 
-# postavi najduzi niz u varijablu
-longestKaladont = kaladonts[longest]
 
-# ispisi najduzi niz u kaladont.txt
-for i in range(len(longestKaladont)):
-    with open('kaladont.txt', 'a') as f:
-        f.write(longestKaladont[i] + ' ')
+for i in range(os.cpu_count()):
+    thread = threading.Thread(target=main, args=(Lines, i, kaladonts))
+    thread.start()
+    
